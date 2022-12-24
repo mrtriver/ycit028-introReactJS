059c1e7543f91f90bdaec7009c6e2b1c32797815
muratirmak <mrtriver@gmail.com>
2022-12-24 16:03:04 -0500

    mui date time pickers install dayjs and datePicker

--- lecture-9-data-entry-app/src/App.jsx
+++ lecture-9-data-entry-app/src/App.jsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 
 // import './App.css' SINCE WE ARE USING MATERIAL UI, WE DON'T NEED THIS
 
@@ -9,7 +10,12 @@ function App() {
       <Box>
         <Typography>Mr. IRMAK</Typography>
         <Typography>Saint Leonard</Typography>
-        
+        <Box>
+        <TextField id="medicare-field" label="Medicare" variant="filled" />
+        </Box>
+        <Box>
+          
+        </Box>
       </Box>
     </Box>
   );
