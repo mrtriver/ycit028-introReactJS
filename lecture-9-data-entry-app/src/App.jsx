d11a484d6dcb4028015f0bec51449553272f2535
muratirmak <mrtriver@gmail.com>
2022-12-24 15:25:31 -0500

    medicare application base setup

---
+++ lecture-9-data-entry-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+// import './App.css' SINCE WE ARE USING MATERIAL UI, WE DON'T NEED THIS
+
+function App() {
+  return (
+    <Box>
+      <Box>
+        <Typography>Mr. IRMAK</Typography>
+        <Typography>Saint Leonard</Typography>
+        
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
